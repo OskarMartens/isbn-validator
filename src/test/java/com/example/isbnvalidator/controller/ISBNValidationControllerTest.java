810c14f266965f1b5bad6408018893feb2412562
OskarMartens <martens.oskar@gmail.com>
2023-10-29 12:48:14 +0100

    Add test for controller.

---
+++ src/test/java/com/example/isbnvalidator/controller/ISBNValidationControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.isbnvalidator.controller;
+
+import com.example.isbnvalidator.service.ISBNValidationService;
+import org.junit.jupiter.api.Test;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(controllers = ISBNValidationController.class)
+@ExtendWith(MockitoExtension.class)
+class ISBNValidationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ISBNValidationService isbnValidationService;
+
+    @Test
+    public void confirm_10_digits_without_x_is_valid() throws Exception {
+
+        String isbnNumber = "0143039431";
+        ResultActions response = mockMvc.perform(post("/api/ISBNValidation/" + isbnNumber));
+        response.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void confirm_incorrect_url_returns_404() throws Exception {
+
+        String isbnNumber = "0143039431";
+        ResultActions response = mockMvc.perform(post("/api/ISBNValidate/" + isbnNumber));
+
+        response.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+}
