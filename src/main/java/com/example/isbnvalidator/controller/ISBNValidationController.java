71672ee3d283bd8b734c29c97baad97b0c3b8359
OskarMartens <martens.oskar@gmail.com>
2023-10-29 12:48:29 +0100

    Add test for controller.

---
+++ src/main/java/com/example/isbnvalidator/controller/ISBNValidationController.java
@@ -0,0 +1,26 @@
+package com.example.isbnvalidator.controller;
+
+import com.example.isbnvalidator.service.ISBNValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ISBNValidation")
+public class ISBNValidationController {
+
+    private final ISBNValidationService isbnValidationService;
+
+    @Autowired
+    public ISBNValidationController(ISBNValidationService isbnValidationService) {
+        this.isbnValidationService = isbnValidationService;
+    }
+
+    @PostMapping("/{ISBNString}")
+    public ResponseEntity<String> validateISBN(@PathVariable ("ISBNString") String ISBNString){
+        return isbnValidationService.validateISBN(ISBNString);
+    }
+}
