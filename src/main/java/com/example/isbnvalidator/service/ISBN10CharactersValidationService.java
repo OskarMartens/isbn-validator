510a3de365b7557a600d151e6fb3efb23bac0293
OskarMartens <martens.oskar@gmail.com>
2023-10-30 08:24:37 +0100

    alternative generic method for 10 digits

--- src/main/java/com/example/isbnvalidator/service/ISBN10CharactersValidationService.java
+++ src/main/java/com/example/isbnvalidator/service/ISBN10CharactersValidationService.java
@@ -10,31 +10,15 @@ public class ISBN10CharactersValidationService {
     static ResponseEntity<String> validate10CharactersISBN(String ISBNString){
 
         if(ISBNString.matches("[0-9]+") && ISBNString.length() == 10 ) {
-            return validate10DigitsISBNWithoutX(ISBNString);
+            return validateISBN(ISBNString, null);
         }
 
         String modifiedISBN = convertUsingStringBuilder(ISBNString);
 
-        if (modifiedISBN.matches("[0-9]+") && modifiedISBN.length() == 11 ) {
-            return validate10ISBNDigitsWithX(modifiedISBN, ISBNString);
+        if (modifiedISBN.matches("[0-9]+") && modifiedISBN.length() == 9 ) {
+            return validateISBN(modifiedISBN, ISBNString);
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The provided string: " + ISBNString + ", does not consist of digits.");
-    }
-
-    private static ResponseEntity<String> validate10DigitsISBNWithoutX(String ISBNString) {
-        int multiplier = 10;
-        int sum = 0;
-
-        for (int i = 0; i < ISBNString.length(); i++) {
-            int digit = Character.getNumericValue(ISBNString.charAt(i));
-            sum += digit * multiplier;
-            multiplier -= 1;
-        }
-
-        if(sum % 11 == 0) {
-            return ResponseEntity.status(HttpStatus.OK).body("The provided string: " + ISBNString + ", is a valid ISBN.");
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The provided string: " + ISBNString + ", is not a valid ISBN.");
     }
 
     private static String convertUsingStringBuilder(String ISBNString) {
@@ -44,24 +28,34 @@ public class ISBN10CharactersValidationService {
         StringBuilder modifiedISBN = new StringBuilder(ISBNString);
         if(endsWithX) {
             modifiedISBN.deleteCharAt(ISBNString.length() -1);
-            modifiedISBN.append("10");
         }
         return modifiedISBN.toString();
     }
 
-    private static ResponseEntity<String> validate10ISBNDigitsWithX(String ISBNString, String ISBNWithX) {
+    private static ResponseEntity<String> validateISBN(String ISBNString, String ISBNWithX){
         int multiplier = 10;
-        int sum = 10;
+        int sum = 0;
 
-        for (int i = 0; i < ISBNString.length() -2; i++) {
+        if (ISBNString.length() == 9){
+            sum += 10;
+        }
+
+        for (int i = 0; i < ISBNString.length(); i++) {
             int digit = Character.getNumericValue(ISBNString.charAt(i));
             sum += digit * multiplier;
             multiplier -= 1;
         }
 
-        if(sum % 11 == 0) {
+        if(sum % 11 == 0 && ISBNWithX == null) {
+            return ResponseEntity.status(HttpStatus.OK).body("The provided string: " + ISBNString + ", is a valid ISBN.");
+        }
+        else if (sum % 11 == 0) {
             return ResponseEntity.status(HttpStatus.OK).body("The provided string: " + ISBNWithX + ", is a valid ISBN.");
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The provided string: " + ISBNWithX + ", is not a valid ISBN.");
+        else if (ISBNWithX == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The provided string: " + ISBNString + ", is not a valid ISBN.");
+        }
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The provided string: " + ISBNWithX + ", is not a valid ISBN.");
     }
 }
